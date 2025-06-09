2836ddf81bf4b3a33fe882ff79940180469d9ce7
carloshmccarlos <carloshmccarlos@gmail.com>
2025-06-09 12:04:29 +0800

    123

--- app/category/[name]/page.tsx
+++ app/category/[name]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import React, { use } from "react";
+import React from "react";
 
 import BigCard from "@/components/BigCard";
 import Footer from "@/components/Footer";
