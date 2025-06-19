1674571ebfa66ce80d63365b5e1042350113612b
carloshmccarlos <carloshmccarlos@gmail.com>
2025-06-19 14:34:20 +0800

    123

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,10 @@
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import dotenv from 'dotenv'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+dotenv.config()
+const connectionString = `${process.env.DATABASE_URL}`
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
+const adapter = new PrismaNeon({ connectionString })
+// @ts-ignore
+export const prisma = new PrismaClient({ adapter })
